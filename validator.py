693fade2ab7a503b6abc68897c1c01963ea4f8d1
Ayush <ayush.beekum@gmail.com>
2026-06-20 13:03:40 +0000

    checkpoint

--- validator.py
+++ validator.py
@@ -1,8 +1,27 @@
 import re
 
 def is_valid_email(email):
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    # determine whether invalid start of string (.. is illegal)
+    no_dot_chars = r'[\w_%+-]'
+    unquoted_local =  rf'{no_dot_chars}+(?:\.{no_dot_chars}+)*'
+    
+    # if wrapped in "" not as strict by RFC but it must end in " or it is illegal
+    quoted_content = r'(?:[^"\\]|\\.)*'
+    quoted_local = rf'"{quoted_content}"'
 
-    if re.match(pattern, email):
+    # or for if "" or not
+    local_part = rf'(?:{unquoted_local}|{quoted_local})'
+
+    # characters only no numbers or underscores etc
+    domain_label = r"[\w](?:[\w-]*[\w])?"
+    tld = r"[^\W\d_]{2,}"
+
+    domain_part = rf"{domain_label}(?:\.{domain_label})*\.{tld}"
+
+    pattern = rf'^{local_part}@{domain_part}$'
+
+    if re.match(email, pattern, re.UNICODE):
         return True
     return False
+
+
